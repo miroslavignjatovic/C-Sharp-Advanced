1acb46a8cc1839bba3adb063cb102813d4f475d5
miroslavignjatovic <130685394+miroslavignjatovic@users.noreply.github.com>
2023-08-16 23:11:48 +0200

    JavaScriptSerializer Homework

---
+++ CasAdv11Homework/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasAdv11Homework
+{
+    public class Course
+    {
+        public string Name { get; set; }
+        public int Grade { get; set; }
+    }
+}
---
+++ CasAdv11Homework/Program.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CasAdv11Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person
+            {
+                Name = "Pera",
+                Surname = "peric",
+                Age = 25,
+                Course = new List<Course>
+                 {
+                    new Course { Name = "csarp", Grade = 9 },
+                    new Course { Name = "javascript", Grade = 10 },
+                    new Course { Name = "sql", Grade = 8 }
+                 }
+            };
+            string json = JsonConvert.SerializeObject(person, Formatting.Indented);
+            Console.WriteLine("Serialized JSON:");
+            Console.WriteLine(json);
+
+            File.WriteAllText("OnePerson.json", json);
+
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::");
+            
+           
+            string jsonData = File.ReadAllText("OnePerson.json");
+            Person deserializedPerson = JsonConvert.DeserializeObject<Person>(jsonData);
+
+            Console.WriteLine("Deserialized Person:");
+            Console.WriteLine($"Name: {deserializedPerson.Name}");
+            Console.WriteLine($"Surname: {deserializedPerson.Surname}");
+            Console.WriteLine($"Age: {deserializedPerson.Age}");
+            Console.WriteLine("Courses:");
+            foreach (var course in deserializedPerson.Course)
+            {
+                Console.WriteLine($"  Course: {course.Name}, Grade: {course.Grade}");
+            }
+
+        }
+    }
+}
+   
